5ffff63012bf936c73cf378e538296b483a00ba0
thaimynguyen <thaimynguyen@gmail.com>
2022-06-29 22:04:37 -0700

    Add proptypes

--- src/components/SelectedCardList.js
+++ src/components/SelectedCardList.js
@@ -1,10 +1,7 @@
 import Card from "./Card";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
-const SelectedCardList = ({ chosenBoard, deleteCard }) => {
-  const cards = chosenBoard.cards;
-  // console.log(cards);
-  // console.log(cards[0]["message"]);
+const SelectedCardList = ({ boardTitle, cards, deleteCard }) => {
   const getCardsJSX = (cards) => {
     return cards.map((card) => (
       <Card
@@ -12,30 +9,30 @@ const SelectedCardList = ({ chosenBoard, deleteCard }) => {
         boardId={card.board_id}
         cardId={card.card_id}
         message={card.message}
+        likeCount={card.likes_count}
         deleteCard={deleteCard}
-        // addHeartCount={addHeart}
-        // addHeart={addHeart}
       />
     ));
   };
   return (
     <div>
-      <h2>Cards for {chosenBoard.title}</h2>
+      <h2>Cards for {boardTitle}</h2>
       <ul>{getCardsJSX(cards)}</ul>
     </div>
   );
 };
 
-// CardList.propTypes = {
-//   cards: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.number.isRequired,
-//       message: PropTypes.string.isRequired,
-//     })
-//   ).isRequired,
-//   addHeartCount: PropTypes.func.isRequired,
-//   addHeart: PropTypes.func.isRequired,
-//   deleteCard: PropTypes.func.isRequired,
-// };
+SelectedCardList.propTypes = {
+  boardId: PropTypes.number.isRequired,
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      board_id: PropTypes.number.isRequired,
+      card_id: PropTypes.number.isRequired,
+      likes_count: PropTypes.number.isRequired,
+      message: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  deleteCard: PropTypes.func.isRequired,
+};
 
 export default SelectedCardList;
