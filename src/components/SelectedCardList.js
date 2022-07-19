1b79b222b28bc39379dc9dba49d670748f6a5b99
Taingleap Chheang <chheangtaingleap@gmail.com>
2022-07-18 19:18:08 -0700

    TC: update SeletedCardList Component by adding sort options

--- src/components/SelectedCardList.js
+++ src/components/SelectedCardList.js
@@ -1,8 +1,9 @@
 import Card from "./Card";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const SelectedCardList = ({ boardTitle, cards, deleteCard, addLike }) => {
-  const cardListJSX = cards
+  const cardListJSXSortById = cards
     .sort((a, b) => a.card_id - b.card_id)
     .map((card) => (
       <Card
@@ -13,10 +14,51 @@ const SelectedCardList = ({ boardTitle, cards, deleteCard, addLike }) => {
       />
     ));
 
+  const cardListJSXSortByAlphabetically = cards
+    .sort((a, b) => a.message.localeCompare(b.message))
+    .map((card) => (
+      <Card
+        key={card.card_id}
+        cardData={card}
+        deleteCard={deleteCard}
+        addLike={addLike}
+      />
+    ));
+
+  const cardListJSXSortByNumberofLikes = cards
+    .sort((a, b) => a.likes_count - b.likes_count)
+    .map((card) => (
+      <Card
+        key={card.card_id}
+        cardData={card}
+        deleteCard={deleteCard}
+        addLike={addLike}
+      />
+    ));
+
+  const [cardListData, setCardListData] = useState(cardListJSXSortById);
+
+  const updateCardListData = () => {
+    const select = document.getElementById("dropDownOptions");
+    if (select.value === "byId") {
+      setCardListData(cardListJSXSortById);
+    } else if (select.value === "byAlphabetically") {
+      setCardListData(cardListJSXSortByAlphabetically);
+    } else if (select.value === "byNumber") {
+      setCardListData(cardListJSXSortByNumberofLikes);
+    }
+  };
+
   return (
     <div>
       <h2>Cards for {boardTitle}</h2>
-      <ul>{cardListJSX}</ul>
+      <label for="sorts">Sort:</label>
+      <select id="dropDownOptions" onChange={updateCardListData}>
+        <option value="byId">Sort by ID</option>
+        <option value="byAlphabetically">Sort alphabetically</option>
+        <option value="byNumber">Sort by number of "+1"s</option>
+      </select>
+      <ul>{cardListData}</ul>
     </div>
   );
 };
